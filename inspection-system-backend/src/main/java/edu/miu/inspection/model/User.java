87dbb2d098016fdb9674188b79c70ebb8f6821d8
Theara Seng <me@thearaseng.com>
2023-08-01 16:55:05 -0500

    add users

--- inspection-system-backend/src/main/java/edu/miu/inspection/model/User.java
+++ inspection-system-backend/src/main/java/edu/miu/inspection/model/User.java
@@ -15,19 +15,87 @@ public class User implements UserDetails {
 
     @Id
     @GeneratedValue
-    public Long id;
-
+    private Long id;
     @NotNull
-    public String email;
-
+    private String email;
     @NotNull
-    public String password;
-
+    private String password;
     @NotNull
-    public String authorities;
-
+    private String authorities;
     @NotNull
-    public Boolean enabled = false;
+    private Boolean enabled = false;
+    @NotNull
+    private String firstName;
+    @NotNull
+    private String lastName;
+    @NotNull
+    private String phone;
+    @NotNull
+    private String location;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAuthorities(String authorities) {
+        this.authorities = authorities;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- inspection-system-backend/src/main/resources/import.sql
+++ inspection-system-backend/src/main/resources/import.sql
@@ -1,4 +1,2 @@
--- Password are hashed using a BCrypt hash with a cost of 10
-insert into user(id, email, password, authorities, enabled) values(1, 'user', '$2a$10$KoxHNdhOe6OY88Ybq6T2d.SGp6lVfj5ynY/QwaO5SRk998TgnYayi', 'USER', true); -- password = 'password' BCrypt 10 hashed
-insert into user(id, email, password, authorities, enabled) values(2, 'admin', '$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi', 'ADMIN, ROOT', true);
-insert into user(id, email, password, authorities, enabled) values(3, 'expired', '$2a$10$PZ.A0IuNG958aHnKDzILyeD9k44EOi1Ny0VlAn.ygrGcgmVcg8PRK', 'USER', true);
+INSERT INTO user (id, first_name, last_name, email, password, phone, location, authorities, enabled) VALUES (1, 'John', 'Doe', 'john@miu.com', '$2a$10$KoxHNdhOe6OY88Ybq6T2d.SGp6lVfj5ynY/QwaO5SRk998TgnYayi', '641890987', '1000 N 4th street, Fairfield, Iowa 52557', 'ADMIN', true);
+INSERT INTO user (id, first_name, last_name, email, password, phone, location, authorities, enabled) VALUES (2, 'Thomas', 'Brown', 'thomas@miu.com', '$2a$10$KoxHNdhOe6OY88Ybq6T2d.SGp6lVfj5ynY/QwaO5SRk998TgnYayi', '641890987', '1000 N 4th street, Fairfield, Iowa 52557', 'INSPECTOR', true);
