ab41481ee3776e4ff439f2d79405b16858e49a29
Theara Seng <me@thearaseng.com>
2023-07-31 22:00:18 -0500

    disable auth for now

--- inspection-system-backend/src/main/java/edu/miu/inspection/security/WebSecurityConfiguration.java
+++ inspection-system-backend/src/main/java/edu/miu/inspection/security/WebSecurityConfiguration.java
@@ -17,7 +17,7 @@ public class WebSecurityConfiguration extends ResourceServerConfigurerAdapter {
                 .csrf()
                 .disable()
                 .authorizeRequests()
-                .antMatchers("/my-public-api/**").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().authenticated();
     }
 
