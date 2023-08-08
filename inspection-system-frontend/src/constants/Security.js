49ecf766f2affec2c27c1508142a24ca449dab93
Thao Duc Vu <34920864+thaok61@users.noreply.github.com>
2023-08-08 14:37:05 -0500

    Thao/implement home screen (#2)
    * implement logout
    
    * update home screen
    
    * show home screen base on role
    
    ---------
    
    Co-authored-by: Vu Duc Thao <thao.vd1712@gmail.com>

--- inspection-system-frontend/src/constants/Security.js
+++ inspection-system-frontend/src/constants/Security.js
@@ -1,2 +1,3 @@
-export const CLIENT_ID = 'myClient';
-export const CLIENT_SECRET = 'myClientSecret';
+export const ROLE_ADMIN = 'ROLE_ADMIN';
+export const ROLE_INSPECTOR = 'ROLE_INSPECTOR';
+export const ROLE_MANAGER = 'ROLE_MANAGER';
