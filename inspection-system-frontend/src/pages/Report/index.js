cffdbefc772e669e476f787498bdf1e3d3e23ead
Thao Duc Vu <34920864+thaok61@users.noreply.github.com>
2023-08-14 22:01:19 -0500

    implement report screen (#6)
    Co-authored-by: Vu Duc Thao <thao.vd1712@gmail.com>

--- inspection-system-frontend/src/pages/Report/index.js
+++ inspection-system-frontend/src/pages/Report/index.js
@@ -1,12 +1,119 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import {Card, Col, message} from "antd";
 import Header from "../../components/Header/Header";
-import {Col} from "antd";
+import {connect, useSelector} from "react-redux";
+import {getTasks, getTasksByManager} from "../../services/Task";
+import {getHireInspector, getUsers} from "../../services/User";
 
-export default function Report() {
+const mapStateToProps = () => ({});
+const Report = (props) => {
+    const [users, setUsers] = useState(0);
+    const {token, role} = useSelector(({auth}) => auth);
+    const arrayRole = JSON.parse(role);
+    const [tasks, setTasks] = useState(0);
+    const fetchTasksForManager = (props) => {
+        props.getTasksByManager(
+            { page: 0, pageSize: 10 },
+            (code, res) => handleResult(code, res)
+        );
+        const handleResult = (code, res) => {
+            if (code === 200) {
+                console.log(res);
+                setTasks(res.totalElements);
+
+            } else {
+                console.log(code);
+                message.error("Error when getting domain information!");
+            }
+        };
+    }
+
+    const fetchTasksForAdmin = () => {
+        props.getTasks(
+            { page: 0, pageSize: 10 },
+            (code, res) => handleResult(code, res)
+        );
+        const handleResult = (code, res) => {
+            if (code === 200) {
+                console.log(res);
+                setTasks(res.totalElements);
+
+            } else {
+                console.log(code);
+                message.error("Error when getting domain information!");
+            }
+        };
+    }
+
+    const fetchUsersByAdmin = () =>  {
+        props.getUsers(
+            { page: 0, pageSize: 10 },
+            (code, res) => handleResult(code, res)
+        );
+
+        const handleResult = (code, res) => {
+            if (code === 200) {
+                console.log(res);
+                setUsers(res.totalElements)
+            } else {
+                console.log(code);
+                message.error("Error when getting domain information!");
+            }
+        };
+    }
+
+    const fetchUsersByManager = () =>  {
+        props.getHireInspector(
+            { page: 0, pageSize: 10 },
+            (code, res) => handleResult(code, res)
+        );
+
+        const handleResult = (code, res) => {
+            if (code === 200) {
+                console.log(res);
+                setUsers(res.totalElements)
+            } else {
+                console.log(code);
+                message.error("Error when getting domain information!");
+            }
+        };
+    }
+
+
+
+    useEffect(() => {
+        if (token != null) {
+            let isAdmin = false;
+            for (const element of arrayRole) {
+                if ("ROLE_ADMIN".includes(element)) {
+                    isAdmin = true;
+                }
+            }
+            if (isAdmin) {
+                fetchTasksForAdmin();
+                fetchUsersByAdmin();
+            } else {
+                fetchTasksForManager(props);
+                fetchUsersByManager();
+            }
+        }
+        // eslint-disable-next-line
+    }, []);
+
+
     return (
         <Col>
         <Header/>
-        Report
-    </Col>
+            <Card title="Report">
+                <p>Number of Users: {users}</p>
+                <p>Number of Tasks: {tasks}</p>
+            </Card>
+        </Col>
     );
 }
+export default connect(mapStateToProps, {
+    getTasksByManager,
+    getTasks,
+    getUsers,
+    getHireInspector
+})(Report);
