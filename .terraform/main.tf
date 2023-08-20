19bf5d0ae88a60853400f88336bbbdc99fd9f578
Theara Seng <me@thearaseng.com>
2023-08-20 11:44:14 -0500

    update amazon ami

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
-  user_data = <<-EOF
+  user_data = <<EOF
 #!/bin/bash
 sudo yum update -y
 sudo yum install docker
