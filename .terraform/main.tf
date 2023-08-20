882b8756155c1af195203c1c7992716da5110f99
Theara Seng <me@thearaseng.com>
2023-08-20 11:50:34 -0500

    update

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -32,6 +32,21 @@ sudo systemctl enable docker
 sudo usermod -a -G docker ec2-user
 sudo systemctl start docker
 EOF
+
+  provisioner "remote-exec" {
+    inline = <<EOF
+      #!/bin/bash
+      sudo yum update -y
+      sudo yum install docker
+      wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
+      sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/libexec/docker/cli-plugins/docker-compose
+      chmod +x /usr/libexec/docker/cli-plugins/docker-compose
+      sudo systemctl enable docker
+      sudo usermod -a -G docker ec2-user
+      sudo systemctl start docker
+    EOF
+  }
+
 }
 
 resource "aws_security_group" "web-sg" {
