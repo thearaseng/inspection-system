302f35d93fc9150d14c3f7ec526334e4749004d0
Theara Seng <me@thearaseng.com>
2023-08-20 11:55:14 -0500

    update

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -22,17 +22,16 @@ resource "aws_instance" "web" {
   vpc_security_group_ids = [aws_security_group.web-sg.id]
 
   provisioner "remote-exec" {
-    inline = <<EOF
-#!/bin/bash
-sudo yum update -y
-sudo yum install docker
-wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)
-sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/libexec/docker/cli-plugins/docker-compose
-chmod +x /usr/libexec/docker/cli-plugins/docker-compose
-sudo systemctl enable docker
-sudo usermod -a -G docker ec2-user
-sudo systemctl start docker
-EOF
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install docker",
+      "wget https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)",
+      "sudo mv docker-compose-$(uname -s)-$(uname -m) /usr/libexec/docker/cli-plugins/docker-compose",
+      "chmod +x /usr/libexec/docker/cli-plugins/docker-compose",
+      "sudo systemctl enable docker",
+      "sudo usermod -a -G docker ec2-user",
+      "sudo systemctl start docker"
+    ]
   }
 
 }
