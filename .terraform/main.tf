85b1e044c0fd0cbd4e1d8c3412bf684f3c024be9
Theara Seng <me@thearaseng.com>
2023-08-20 14:28:33 -0500

    update

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -20,7 +20,6 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.amazon-linux.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  key_name               = "SoftwareEngineering"
 
   user_data = <<EOF
 #!/bin/bash
